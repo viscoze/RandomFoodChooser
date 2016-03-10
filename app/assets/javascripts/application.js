6e735d6bcf97340c7ac4772b96263a0f16b732f4
Viscoze <vlad.oganov@gmail.com>
2016-03-10 20:35:34 +0300

    React works well

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
 //= require jquery
 //= require bootstrap-sprockets
 //= require react
--- app/assets/javascripts/components/cuisine_box.js.jsx
+++ app/assets/javascripts/components/cuisine_box.js.jsx
@@ -1,5 +1,8 @@
 var ChosenCuisineBox = React.createClass({
   render: function() {
+    if (this.props.name === undefined) {
+      return (<div className="cuisine-box empty-box">Choose Something</div>)
+    }
     var image = "cuisine/" + this.props.name + ".jpg"
 
     return (
@@ -17,8 +20,29 @@ var ChosenCuisineBox = React.createClass({
   }
 });
 
-var X = React.createClddass({
-  render: () => {
-    return (<span className="glyphicon glyphicon-grain"></span>);
+var Cuisine = React.createClass({
+
+  clickOnCuisine(event) {
+    ReactDOM.render(<ChosenCuisineBox name={this.props.name}/>,
+                    document.getElementById('chosen'));
+  },
+
+  render(){
+    var image = "app/assets/images/cuisine/" + this.props.name + ".jpg"
+
+    return (
+    <div className="cuisine-box" onClick={this.clickOnCuisine}>
+      <div className="row">
+        <div className="col-sm-2">
+          <img src={image} />
+        </div>
+        <div className="col-sm-9">
+          <span className="name">{this.props.name}</span><br />
+          <span className="description">{this.props.description}</span>
+        </div>
+      </div>
+    </div>
+    )
   }
+
 })
